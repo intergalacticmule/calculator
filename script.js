2fee5d586878978228bd0d2b44fdb428d3a15c2f
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2024-03-05 16:30:37 +0200

    Add button selectors

---
+++ script.js
@@ -0,0 +1,22 @@
+const screenContent = document.querySelector("#screen-container");
+
+const clearButton = document.querySelector("#clear-all");
+const deleteButton = document.querySelector("#delete");
+
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const zeroButton = document.querySelector("#zero");
+
+const decimalButton = document.querySelector("#decimal");
+const equalsButton = document.querySelector("#equals");
+const additionButton = document.querySelector("#addition");
+const subtractionButton = document.querySelector("#subtraction");
+const multiplicationButton = document.querySelector("#multiplication");
+const divisionButton = document.querySelector("#division");
